8622f485c6ba29130d88e7bb236a2771f524f6ab
marthaberner <marthaberner.mb@gmail.com>
2016-01-06 11:23:45 -0700

    remove monk

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var db = require('monk')('localhost/hello-demo');
-var carsCollection = db.get('cars');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
