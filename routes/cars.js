cab017b82ce844328181739370158dbad6ecb0c4
april sikes <aprilsikes@gmail.com>
2016-01-06 19:03:05 -0700

    car routes

--- routes/cars.js
+++ routes/cars.js
@@ -1,7 +1,34 @@
 var express = require('express');
 var router = express.Router();
-
+var carId;
 
 // add your routes here
+router.get("/cars", function (req, res) {
+  res.render("cars/index")
+});
+
+router.get("/cars/new", function (req, res) {
+  res.render("cars/new")
+});
+
+router.post("/cars", function (req, res) {
+  res.redirect("/cars")
+});
+
+router.get("/cars/:id", function (req, res) {
+  res.render("cars/show", { carId: req.params.id })
+});
+
+router.get("/cars/:id/edit", function (req, res) {
+  res.render("cars/edit", { carId: req.params.id })
+});
+
+router.post("/cars/:id", function (req, res) {
+  res.redirect("/cars", { carId: req.params.id })
+});
+
+router.post("/cars/:id/delete", function (req, res) {
+  res.redirect("/cars", { carId: req.params.id })
+});
 
 module.exports = router;
