69f77c07b651c5354383111ded60962cee9fe320
april sikes <aprilsikes@gmail.com>
2016-03-21 16:16:06 -0600

    committing

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var cars = require('./routes/cars');
+// var cars = require('./routes/cars');
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/', cars);
+// app.use('/', cars);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+var pg = require('pg');
+
+var connectionString = 'postgres://localhost/postgres';
+
+function runQuery (query, callback) {
+  pg.connect(connectionString, function (err, client, done) {
+    if (err) { done(); console.log(err); return; }
+    client.query(query, function (err, results) {
+      done();
+      if (err) { console.log(err); return; }
+      callback(results);
+    });
+  });
+}
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  runQuery('SELECT * FROM cars', function (results) {
+    res.send(results.rows);
+  });
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
